1efe5af0ec6fe086e63e3099d769d5e0cda1e114
Villalba-carla84 <carlavillalba84@hotmail.com>
2023-08-30 18:23:23 -0300

    correccion task7

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -3,23 +3,18 @@ import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.AccountUtil;
 import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.models.RolUser;
 import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Random;
 
 import static java.util.stream.Collectors.toList;
 
--- src/main/java/com/mindhub/homebanking/models/RolUser.java
+++
@@ -1,6 +0,0 @@
-package com.mindhub.homebanking.models;
-
-public enum RolUser {
-    CLIENT,
-    ADMIN
-}
